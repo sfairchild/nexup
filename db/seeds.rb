1e2f8813a106fda646f3d69d8c141e43de23b5e7
Sean <seanf215@gmail.com>
2017-10-22 13:14:03 -0400

    Adjust positions

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@ BattleUser.delete_all
 default_angle = Angle.create(pivot: 90)
 Game.create(name: 'default', angle_id: default_angle.id)
 
-tv = Angle.create(pivot: 20)
+tv = Angle.create(pivot: 90)
 Game.create(name: 'halo',     angle: tv)
 Game.create(name: 'smash',    angle: tv)
-Game.create(name: 'magic',    angle: Angle.new(pivot: 180))
-Game.create(name: 'pong',     angle: Angle.new(pivot: 120))
-Game.create(name: 'foosball', angle: Angle.new(pivot: 40))
+Game.create(name: 'magic',    angle: tv)
+Game.create(name: 'pong',     angle: Angle.new(pivot: 65))
+Game.create(name: 'foosball', angle: Angle.new(pivot: 150))
--- py_scripts/gifcam.py
+++ py_scripts/gifcam.py
@@ -14,7 +14,7 @@ import sys
 #
 ########################
 num_frame = 4       # Number of frames in Gif
-gif_delay = 10      # Frame delay [ms]
+gif_delay = 15      # Frame delay [ms]
 
 ########################
 #
@@ -23,25 +23,23 @@ gif_delay = 10      # Frame delay [ms]
 ########################
 camera = picamera.PiCamera()
 camera.resolution = (540, 405)
-camera.rotation = 90
-camera.brightness = 70
+camera.rotation = 0
+# camera.brightness = 70
 camera.image_effect = 'none'
 # camera.zoom = (sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
 
 try:
       print('Gif capture Started')
-      for i in range(num_frame):
+      for i in range(num_frame + 5):
+          time.sleep(.25)
           camera.capture('{0:04d}.jpg'.format(i))
+
       print('Gif capture done')
 
       ### PROCESSING GIF ###
-      for i in range(num_frame - 1):
-          source = str(num_frame - i - 1) + ".jpg"
-          source = source.zfill(8) # pad with zeros
-          # dest = str(num_frame + i) + ".jpg"
-          # dest = dest.zfill(8) # pad with zeros
-          # copyCommand = "cp " + source + " " + dest
-          # os.system(copyCommand)
+      for i in range(num_frame - 2):
+          source = str(num_frame - i + 5) + ".jpg"
+          source = source.zfill(8)
                         
       filename = '/home/pi/app/public/images/gifs/' + sys.argv[1]
       graphicsmagick = "gm convert -delay " + str(gif_delay * 3) + " " + "*.jpg " + filename

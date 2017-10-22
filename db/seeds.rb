b2c960609a3d0eeed148f04cd6a3f79fc72b276a
Sean <seanf215@gmail.com>
2017-10-22 02:19:28 -0400

    rename seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,5 @@ Game.delete_all
 Angle.delete_all
 
 default_angle = Angle.create(pivot: 90)
-default = Game.new(name: 'default', angle: default_angle)
+default = Game.create(name: 'default', angle_id: default_angle.id)
 
